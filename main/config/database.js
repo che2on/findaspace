7b81c07360d24d3f807259ef33d22fa27dfb3b74
chethan <che4on@gmail.com>
2014-04-07 22:59:21 +0530

    revert to auth

--- main/config/database.js
+++ main/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/protes' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://localhost:27017/auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
